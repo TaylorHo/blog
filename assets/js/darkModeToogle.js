dfe0aeef1f025782bee1efda0ddb8af329df187a
Shiro Neko <tagiki5@gmail.com>
2021-01-28 20:34:50 -0300

    Dark Mode Adjusts

--- assets/js/darkModeToogle.js
+++ assets/js/darkModeToogle.js
@@ -1,15 +1,23 @@
 var html = document.querySelector("html");
+var moon = document.getElementById("moon");
+var sun = document.getElementById("sun");
+
 if(localStorage.getItem('theme') == 'dark'){
   html.classList.add("dark_mode");
+  sun.classList.remove("d-none");
+  moon.classList.add("d-none");
 }
 
 function toogleColor() {
-  var html = document.querySelector("html");
   if(html.classList.contains("dark_mode")){
     html.classList.remove("dark_mode");
+    sun.classList.add("d-none");
+    moon.classList.remove("d-none");
     localStorage.setItem('theme', 'light');
   } else {
     html.classList.add("dark_mode");
+    sun.classList.remove("d-none");
+    moon.classList.add("d-none");
     localStorage.setItem('theme', 'dark');
   }
 }
